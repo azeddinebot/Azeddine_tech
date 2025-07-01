57e6a1af943ce4f5affb1110278d75ab71bcebca
azeddinebot <azeddinep00@gmail.com>
2025-07-01 19:34:11 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+```js
+const { Client} = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const client = new Client();
+
+client.on("qr", qr => {
+  qrcode.generate(qr, { small: true});
+});
+
+client.on("ready", () => {
+  console.log("✅ البوت جاهز للعمل!");
+});
+
+client.on("message", async message => {
+  if (message.body === "/start") {
+    message.reply("مرحبًا بك! أرسل رابط لتحميل الملف.");
+}
+});
+
+client.initialize();
+```
