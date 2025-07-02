d3aad3154c208c1abc47ec6619c5b2efdcec10d5
azeddinebot <azeddinep00@gmail.com>
2025-07-02 19:45:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
 
 client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-  console.log('✅ بوت azeddine_tech جاهز للعمل!');
+    console.log('✅ تم تشغيل بوت azeddine_tech');
 });
 
 client.on('message', async message => {
-  if (message.body === '/start') {
-    message.reply('مرحباً! هذا هو بوت azeddine_tech. أرسل لي رابط لتحميل الملف.');
-  }
+    if (message.body === '/start') {
+        message.reply('مرحباً هذا هو بوت azeddine_tech.\nأرسل لي رابط لتحميل الملف.');
+    }
 });
 
 client.initialize();
